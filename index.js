543eea9876fcb8ce7453605e55aff7f9e8703db2
Ikenna Noble Asiegbulam <101875015+Headaddi@users.noreply.github.com>
2024-04-20 20:54:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const form = document.querySelector("form");
-const weightInput = document.querySelector("#weight");
-const heightInput = document.querySelector("#height");
-const resultDiv = document.querySelector("#result");
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const weight = parseInt(weightInput.value);
-    const height = parseInt(heightInput.value)/100;
-    const bmi = (weight/Math.pow(height,2)).toFixed(2);
-    if (bmi < 18.5) {
-        resultDiv.innerHTML = "Your BMI is "+bmi+", so you are underweight.";
-    } else if (bmi >= 18.5 && bmi<= 24.9){
-        resultDiv.innerHTML = "Your BMI is "+bmi+", so you have a normal weight.";
-    }else if(bmi > 24.9){
-        resultDiv.innerHTML = "Your BMI is "+bmi+", so you are overweight.";
+const inputBox = document.getElementById("inputBox");
+const taskListContainer = document.getElementById("taskListContainer");
+
+
+function addNewTask(){
+    if (inputBox.value === '') {
+        inputBox.placeholder = "Please add a task."
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        taskListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    weightInput.value = '';
-    heightInput.value = '';
-    
-})
+    inputBox.value = '';
+    saveData()
+}
+
+taskListContainer.addEventListener("click", function(e){
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem();
+}
+showTask();
