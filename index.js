573b5ac0481b1e40900af8f866526f40af83b7a0
Ikenna Noble Asiegbulam <101875015+Headaddi@users.noreply.github.com>
2024-04-20 21:47:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const display = document.querySelector("#display");
+const form = document.querySelector("form");
+const weightInput = document.querySelector("#weight");
+const heightInput = document.querySelector("#height");
+const resultDiv = document.querySelector("#result");
 
-function appendToDisplay(input){
-      display.value += input;
-}
-
-function clearAll(){
-    display.value = '';
-}
-
-function solve(){
-    try {
-        display.value = eval(display.value);
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const weight = parseInt(weightInput.value);
+    const height = parseInt(heightInput.value)/100;
+    const bmi = (weight/Math.pow(height,2)).toFixed(2);
+    if (bmi < 18.5) {
+        resultDiv.innerHTML = "Your BMI is "+bmi+", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi<= 24.9){
+        resultDiv.innerHTML = "Your BMI is "+bmi+", so you have a normal weight.";
+    }else if(bmi > 24.9){
+        resultDiv.innerHTML = "Your BMI is "+bmi+", so you are overweight.";
     }
-     catch (error) {
-       display.value = "error" ;
-    }
-}   
+    weightInput.value = '';
+    heightInput.value = '';
+    
+})
