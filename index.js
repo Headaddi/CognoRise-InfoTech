d6969ddf443937cb919f0e9ea238213d4ada2848
Ikenna Noble Asiegbulam <101875015+Headaddi@users.noreply.github.com>
2024-04-20 21:54:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const display = document.querySelector("#display");
 
-function appendToDisplay(input){
-      display.value += input;
+const inputBox = document.getElementById("inputBox");
+const taskListContainer = document.getElementById("taskListContainer");
+
+
+function addNewTask(){
+    if (inputBox.value === '') {
+        inputBox.placeholder = "Please add a task."
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        taskListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData()
 }
 
-function clearAll(){
-    display.value = '';
-}
+taskListContainer.addEventListener("click", function(e){
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
 
-function solve(){
-    try {
-        display.value = eval(display.value);
-    }
-     catch (error) {
-       display.value = "error" ;
-    }
-}   
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem();
+}
+showTask();
